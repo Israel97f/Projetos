cb579bebd3689099dbe420032f47692670b67e0d
Israel Farias <israelfarias97@gmail.com>
2021-10-03 23:05:30 -0300

    uppdate

--- KerballPython/HallKerball.py
+++ KerballPython/HallKerball.py
@@ -2,11 +2,12 @@ import lib.fases
 import lib.Mathe
 
 
+
 # Inicinado a conexão
-lib.fases.IntConect()
-lib.Mathe.Contador(10)
-lib.fases.Lauch()
-lib.fases.Orbitador()
+#lib.fases.IntConect()
+#lib.Mathe.Contador(10)
+#lib.fases.Lauch()
+#lib.fases.Orbitador()
 
 print('Tudo ok')
 #pip install krpc
