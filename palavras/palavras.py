76da6b4784d125c53809cec20c23f74842f5c23b
Israel Farias <israelfarias97@gmail.com>
2021-12-04 11:20:40 -0300

    update

--- palavras/palavras.py
+++ palavras/palavras.py
@@ -3,14 +3,35 @@ import random
 import leitura
 
 arq = 'palavras.txt'
-n = random.randint(0, 100)
-nun = n
+lista_sorteada = ['a', 'b']
+
 if (not leitura.arquivoExite(arq)):
     leitura.criarArquivo(arq)
-lista_de_palavras = leitura.lerArquivo(arq)
-n = int ((n/100) * len(lista_de_palavras))
 
-print(f'{" A palavra sortiada é":^40}')
-print(f'{n}----{nun}----{len(lista_de_palavras)}')
-print(lista_de_palavras[n - 1])
-print('~~' * 20)
+lista_de_palavras = leitura.lerArquivo(arq)
+while True:
+    
+    n = random.randint(0, 100)
+    n = int ((n/100) * len(lista_de_palavras))
+    if len(lista_sorteada) == len(lista_de_palavras):
+        print('Não há mais palavras')
+        break
+
+    if lista_de_palavras[n - 1] in lista_sorteada:
+        continue
+
+    lista_sorteada.append(lista_de_palavras[n - 1])
+        
+
+    print(f'{" A palavra sortiada é ":=^40}')
+    print(f'{lista_de_palavras[n - 1]:^40}'.upper())
+    print('~~' * 20)
+
+    while True:
+        n = str(input('Quer continuar [S/N]:')).upper().strip()
+        if n in "SN":
+            break
+        else:
+            print('comando invalido por favor digite apenas S ou N')
+    if n == 'N':
+        break
