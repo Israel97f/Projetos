4fcfb38e71b2f2e3ed694f9eac93512c6a15764f
Israel Farias <israelfarias97@gmail.com>
2022-05-09 21:38:26 -0300

    Update test.py

--- KerballPython/test.py
+++ KerballPython/test.py
@@ -1,25 +1,109 @@
-lista1 = (1,2,3,4,5,6)
-lista2 = ('a','b','c','d','e', 'f')
+from tkinter import Frame, Tk, font, ttk, Text
+from ttkbootstrap import Style
+from time import sleep
 
-cont = 0
-for v in lista1:
-   
-    print (lista2[cont],v)
-    cont += 1
-
-
-
-def função_1():
-    print('função 1 hey')
-
-def função_2(parametro=None):
-    if parametro != None:
-        parametro()
-
-
-função_2(função_1)
+def função_1(display):
+    
+    i = 0
+    lista = list()
+    while i < 15:
+        
+        i += 1
+        lista.append(i)
+        if len(lista) > 4:
+            atualiza_display(display ,lista[:])
+            lista.clear()
+        display.after (1000)
 
 
+def função_2():
+    pass
 
-for c in range(0, 15, +1):
-    print('o1')
+
+def atualiza_display(display, lista):
+    
+    list = lista[:]
+    names = ('apoastro:', 
+    'periastro:', 
+    'altitude:',
+    'surface_altitude:',
+    'Speed:',
+    'vertical_speed:',
+    'horizontal_speed:',
+    'surface_gravity:')
+
+    treeSelect = (display.identify('item', 0, 20),
+    display.identify('item', 0, 25),
+    display.identify('item', 0, 45),
+    display.identify('item', 0, 65),
+    display.identify('item', 0, 85))
+    for c in treeSelect:
+        if c != '':
+            display.delete(c)
+          
+    
+    cont = 0
+    for c in list:        
+        display.insert("",
+            "end", values=(names[cont], c)
+        )
+        print(names[cont], c)
+        cont +=1
+ 
+
+def main_frame(frame_atual=None):
+    if frame_atual != None:
+        frame_atual.destroy()
+        
+    frame = ttk.Frame()
+    painel_frame = ttk.Frame(frame)
+    display_frame  = ttk.Frame(frame)
+    display = ttk.Treeview(display_frame, columns=('nomes', 'valores'), show='tree',height=8)
+    display.column('#0', minwidth=0,width=10)
+    display.column('nomes', minwidth=0,width=100)
+    display.column('valores', minwidth=0,width=100) 
+    
+    b1 = ttk.Button(painel_frame, text='conectar', width=15, command= lambda:função_1(display))
+    b2 = ttk.Button(painel_frame, text='Orbitar', width=15, command= lambda: função_1(display))
+    b3 = ttk.Button(painel_frame, text='Pousar', width=15, command= lambda: função_1(display))
+    b1.grid(row=0, column=0, padx=10, pady=10)
+    b2.grid(row=1, column=0, padx=10, pady=10)
+    b3.grid(row=2, column=0, padx=10, pady=10)
+    display.grid(row=0, column=1, padx=10, pady=10)
+    painel_frame.grid(row=0, column=0)
+    display_frame.grid(row=0, column=1) 
+    frame.pack()
+
+
+def screen_1(frame_atual=None):
+    if frame_atual != None:
+        frame_atual.destroy()   
+        
+    frame = ttk.Frame()
+    painel_frame = ttk.Frame(frame)
+    display_frame  = ttk.Frame(frame)
+    display = ttk.Treeview(display_frame, columns=('nomes', 'valores'), show='tree',height=8)
+    display.column('#0', minwidth=0,width=10)
+    display.column('nomes', minwidth=0,width=100)
+    display.column('valores', minwidth=0,width=100)  
+    
+    b1 = ttk.Button(painel_frame, text='Lançar', width=15, command=lambda: função_1(display))
+    b2 = ttk.Button(painel_frame, text='voltar', width=15, command=lambda: main_frame(frame))
+    #b3 = ttk.Button(painel_frame, text='B3', width=15, command=tela1)
+    b1.grid(row=0, column=0, padx=10, pady=10)
+    b2.grid(row=1, column=0, padx=10, pady=10)
+    #b3.grid(row=2, column=0, padx=10, pady=10)
+    display.grid(row=0, column=1, padx=10, pady=10)
+    painel_frame.grid(row=0, column=0)
+    display_frame.grid(row=0, column=1)
+    frame.pack()
+    #frame.tkraise()
+
+
+style = Style()
+janela = style.master
+#janela.geometry('350x190')
+janela.title('Hall')
+main_frame()
+janela.mainloop()
+    
