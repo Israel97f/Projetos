5ef8b39b31b7dc00ad5b0781ee7020afec626de0
Israel Farias <israelfarias97@gmail.com>
2022-07-19 01:12:45 -0300

    update

--- yue lua/ex001/ex001.lua
+++ yue lua/ex001/ex001.lua
@@ -1,11 +1,19 @@
-local gui = require "yue.gui"
-local janela = gui.Window.create()
-janela.onclose = gui.MessageLoop.quit
-janela:settitle("meu primeiro código yue")
+-- caminho do yue
+package.cpath = 'yue-lua-5-1/yue.so'
+
+local gui = require 'yue.gui'
+
+-- cria e exibe uma janela
+local janela = gui.Window.create{}
+janela.onclose = function() gui.MessageLoop.quit() end
+janela:settitle("hello wold")
+janela:setcontentview(gui.Label.create("meu primeiro código yue"))
 janela:setcontentsize{
     width = 400,
     height = 600
 }
 janela:center()
-janela:actvate()
+janela:activate()
+
+ -- exibe a mensagem
 gui.MessageLoop.run()
